92240c2150f9ceb0bce24b222195751751879f61
Louay Abacha <6313901@edu.vaniercollege.qc.ca>
2025-04-29 15:02:24 -0400

    ADD SELECTIONSORT IN MAIN

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,21 +6,36 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-     int[] nums ={3,1 ,5,2,1,4};
-     bubbleSort(nums);
+        int[] nums = {3, 1, 5, 2, 1, 4};
+        bubbleSort(nums);
         System.out.println(Arrays.toString(nums));
 
 
     }
-    public static void bubbleSort(int[] nums) {
-          for( int i= 0; i< nums.length -1 ;i++){
-              for (int j = 0; j < nums.length-1 -i ; j++){
-                  //swap
-                  int temp = nums[j];
-                  nums[j] = nums[j +1];
-                  nums[j +1 ] = temp;
-              }
 
+    public static void bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                //swap
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+
+        }
+    }
+
+    public static void selectionSort(int[] nums) {
+        for ( int i = 0 ;i<nums.length -1 ; i++) {
+            int minidx = 1;
+            for ( int j = i +1; i < nums.length ; j++) {
+                if( nums[j] < nums[minidx]) {
+                    minidx = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minidx];
+            nums[minidx] = temp ;
         }
     }
 }
